7ef069304b404331d26a51f07bf0cfbebc9fc7c4
RottenFishFantasy1988 <makiasagawa@gmail.com>
2019-01-16 07:21:23 -0800

    zoom committed out

--- app/src/main/java/com/example/takeawalk/MapsActivity.java
+++ app/src/main/java/com/example/takeawalk/MapsActivity.java
@@ -92,7 +92,7 @@ public class MapsActivity extends AppCompatActivity implements GoogleMap.OnMyLoc
             @Override
             public void onLocationChanged(Location location) {
                 // update the marker if location changes
-                mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(location.getLatitude(), location.getLongitude()), 12));
+                // mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(location.getLatitude(), location.getLongitude()), 12));
                 // get current user speed
                 Data.speed=location.getSpeedAccuracyMetersPerSecond();
                 // report speed
